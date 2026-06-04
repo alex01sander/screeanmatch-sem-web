7d9546f30513f1019fbe21f536d2860288f32b2b
Alex Britto <alexsander01@hotmail.com.br>
2026-06-03 23:46:34 -0300

    refactor: reorganizar fluxo de busca de séries e temporadas

--- screanmatch/src/main/java/br/com/alura/screanmatch/ScreanmatchApplication.java
+++ screanmatch/src/main/java/br/com/alura/screanmatch/ScreanmatchApplication.java
@@ -1,31 +1,28 @@
 package br.com.alura.screanmatch;
 
-import br.com.alura.screanmatch.model.DadosEpisodios;
+
 import br.com.alura.screanmatch.model.DadosSerie;
+import br.com.alura.screanmatch.model.DadosTemporada;
+import br.com.alura.screanmatch.principal.Principal;
 import br.com.alura.screanmatch.service.ConsumoApi;
 import br.com.alura.screanmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ScreanmatchApplication implements CommandLineRunner {
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Seano=1&apikey=60221604");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Seano=1&apikey=60221604");
-		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodios);
-
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScreanmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
--- screanmatch/src/main/java/br/com/alura/screanmatch/model/DadosTemporada.java
+++ screanmatch/src/main/java/br/com/alura/screanmatch/model/DadosTemporada.java
@@ -4,8 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
-@JsonIgnoreProperties(ignoreUnknown = true)
 
-public record DadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episode") List<DadosEpisodios> episodiosList) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(
+        @JsonAlias("Season") Integer numero,
+        @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
