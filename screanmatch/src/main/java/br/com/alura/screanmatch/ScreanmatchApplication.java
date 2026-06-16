cbf7a98d65b9b1978b9f84fa5ab949b77211614e
Alex Britto <alexsander01@hotmail.com.br>
2026-06-16 00:47:58 -0300

    Projeto ScreenMatch

---
+++ screanmatch/src/main/java/br/com/alura/screanmatch/ScreanmatchApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.screanmatch;
+
+
+import br.com.alura.screanmatch.model.DadosSerie;
+import br.com.alura.screanmatch.model.DadosTemporada;
+import br.com.alura.screanmatch.principal.Principal;
+import br.com.alura.screanmatch.repository.SerieRepository;
+import br.com.alura.screanmatch.service.ConsumoApi;
+import br.com.alura.screanmatch.service.ConverteDados;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ScreanmatchApplication implements CommandLineRunner {
+	@Autowired
+	private SerieRepository repositorio;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreanmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+	}
+
+}
