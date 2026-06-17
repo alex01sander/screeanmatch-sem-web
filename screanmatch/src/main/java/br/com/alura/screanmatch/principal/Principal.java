255e97839bedd72f48e6a5c546706de5b7a557eb
Alex Britto <alexsander01@hotmail.com.br>
2026-06-17 18:37:44 -0300

    feat: Buscando serie por ator e titulo.

--- screanmatch/src/main/java/br/com/alura/screanmatch/principal/Principal.java
+++ screanmatch/src/main/java/br/com/alura/screanmatch/principal/Principal.java
@@ -36,7 +36,8 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
-                                    
+                    4 - Buscar Série por titulo   
+                    5 - Buscar Série por ator              
                     0 - Sair                                 
                     """;
 
@@ -54,6 +55,12 @@ public class Principal {
                 case 3:
                     listarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriePorAtor();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -62,6 +69,9 @@ public class Principal {
             }
         }
     }
+
+
+
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
@@ -84,20 +94,22 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series.stream().filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase())).findFirst();
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if(serie.isPresent()){
             var serieEncontrada =  serie.get();
             List<DadosTemporada> temporadas = new ArrayList<>();
 
             for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
-                var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
+                var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().
+                        replace(" ", "+") + "&season=" + i + API_KEY);
                 DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
                 temporadas.add(dadosTemporada);
             }
             temporadas.forEach(System.out::println);
             List<Episodio> episodios = temporadas.stream()
-                    .flatMap(d -> d.episodios().stream().map(e -> new Episodio(d.numero(), e)))
+                    .flatMap(d -> d.episodios().stream()
+                            .map(e -> new Episodio(d.numero(), e)))
                     .collect(Collectors.toList());
             serieEncontrada.setEpisodios(episodios);
             repositorio.save(serieEncontrada);
@@ -109,5 +121,29 @@ public class Principal {
     private void listarSeriesBuscadas(){
         series = repositorio.findAll();
         series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma Série pelo nome");
+        var nomeSerie = leitura.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("Daddos da série" + serieBuscada.get());
+        }else{
+            System.out.println("Nenhum serie encontrado");
+        }
+    }
+
+    private void buscarSeriePorAtor() {
+        System.out.println("Escolha um Ator");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de qual valor ? ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que: " + nomeAtor + " trabalhou ");
+
+        seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + " avaliação " + s.getAvaliacao()));
+
     }
 }
